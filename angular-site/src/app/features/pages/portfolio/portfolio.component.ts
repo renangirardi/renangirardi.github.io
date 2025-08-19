76b98877b449e47324deccb0b8e9a8d976933b10
Renan Girardi <63652821+renangirardi@users.noreply.github.com>
2025-08-19 18:13:32 -0300

    Add next page prompt to portfolio component
    Introduced the NextPageComponent to the portfolio page, prompting users to visit the contact page after viewing the portfolio. Updated imports and template to include the new component.

--- angular-site/src/app/features/pages/portfolio/portfolio.component.ts
+++ angular-site/src/app/features/pages/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InnerBannerComponent } from '../../components/inner-banner/inner-banner.component';
 import { InnerWipContentComponent } from '../../components/inner-wip-content/inner-wip-content.component';
+import { NextPageComponent } from '../../components/next-page/next-page.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [InnerBannerComponent, InnerWipContentComponent],
+  imports: [InnerBannerComponent, InnerWipContentComponent, NextPageComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
