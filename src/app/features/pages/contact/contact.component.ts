074f42ee90e2e02f496e4cedd1a3aa64ed17be32
Renan Girardi <63652821+renangirardi@users.noreply.github.com>
2025-08-23 14:02:22 -0300

    feat: new loading indicator on linkedin badge

--- src/app/features/pages/contact/contact.component.ts
+++ src/app/features/pages/contact/contact.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { InnerBannerComponent } from '../../components/inner-banner/inner-banner.component';
+import { LoadingIndicatorComponent } from '../../../shared/components/loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [InnerBannerComponent],
+  imports: [InnerBannerComponent, CommonModule, LoadingIndicatorComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit {
+  loading = true;
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    const observer = new MutationObserver(() => {
+      const iframe = this.elementRef.nativeElement.querySelector('iframe');
+      if (iframe) {
+        iframe.onload = () => {
+          iframe.style.width = '340px';
+          iframe.style.height = '271px';
+          this.loading = false;
+        };
+      }
+    });
+
+    observer.observe(this.elementRef.nativeElement, {
+      childList: true,
+      subtree: true,
+    });
+  }
 }
---
+++ src/app/shared/components/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  imports: [],
+  templateUrl: './loading-indicator.component.html',
+  styleUrl: './loading-indicator.component.css'
+})
+export class LoadingIndicatorComponent {
+
+}
