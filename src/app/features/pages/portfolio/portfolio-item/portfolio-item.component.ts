af5831fb525c0e09127c342458e885e38ec97b2a
Renan Girardi <63652821+renangirardi@users.noreply.github.com>
2025-08-29 17:15:42 -0300

    feat: start image slider component

--- src/app/features/pages/portfolio/portfolio-item/portfolio-item.component.ts
+++ src/app/features/pages/portfolio/portfolio-item/portfolio-item.component.ts
@@ -6,10 +6,17 @@ import { PortfolioService } from '../../../../core/services/portfolio.service';
 import { InnerBannerComponent } from '../../../components/inner-banner/inner-banner.component';
 import { CommonModule } from '@angular/common';
 import { TagComponent } from '../../../../shared/components/tag/tag.component';
+import { ImageSliderComponent } from '../../../../shared/components/image-slider/image-slider.component';
 
 @Component({
   selector: 'app-portfolio-item',
-  imports: [InnerBannerComponent, CommonModule, TagComponent],
+  imports: [
+    InnerBannerComponent,
+    CommonModule,
+    TagComponent,
+    ImageSliderComponent,
+    ImageSliderComponent,
+  ],
   templateUrl: './portfolio-item.component.html',
   styleUrl: './portfolio-item.component.css',
 })
---
+++ src/app/shared/components/image-slider/image-slider.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider',
+  imports: [CommonModule],
+  templateUrl: './image-slider.component.html',
+  styleUrl: './image-slider.component.css',
+})
+export class ImageSliderComponent implements AfterViewInit {
+  @Input() imagesSrc!: string[];
+  @ViewChildren('image') images!: QueryList<ElementRef<HTMLElement>>;
+  @ViewChildren('dot') dots!: QueryList<ElementRef<HTMLElement>>;
+  slideIndex = 1;
+
+  ngAfterViewInit() {
+    this.showImage(this.slideIndex);
+  }
+
+  nextImage(index: number) {
+    this.showImage((this.slideIndex += index));
+  }
+
+  chooseImage(index: number) {
+    this.showImage((this.slideIndex = index));
+  }
+
+  showImage(index: number) {
+    const imagesArray = this.images.toArray();
+    const dotsArray = this.dots.toArray();
+
+    if (index > imagesArray.length) {
+      this.slideIndex = 1;
+    }
+    if (index < 1) {
+      this.slideIndex = imagesArray.length;
+    }
+
+    imagesArray.forEach((img) => (img.nativeElement.style.display = 'none'));
+
+    dotsArray.forEach((dot) => {
+      dot.nativeElement.className = dot.nativeElement.className.replace(
+        ' image-slider__dot--active',
+        '',
+      );
+    });
+
+    imagesArray[this.slideIndex - 1].nativeElement.style.display = 'block';
+    dotsArray[this.slideIndex - 1].nativeElement.className +=
+      ' image-slider__dot--active';
+  }
+}
